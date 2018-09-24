fbd45f8567526b0cd572dc65232c29b22a1573e8
douglas-augusto <douglas-augusto@hotmail.com>
2018-09-24 14:42:58 -0300

    Pacotes criados, Activities criadas, ConfigFirebase Criada, Helper Base64, Helper Preferencias, Models Contato e Usuario

--- app/src/main/java/com/example/douglas/agendafirebase/activity/MainActivity.java
+++ app/src/main/java/com/example/douglas/agendafirebase/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.douglas.agendafirebase;
+package com.example.douglas.agendafirebase.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.douglas.agendafirebase.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/douglas/agendafirebase/config/ConfiguracaoFirebase.java
@@ -0,0 +1,28 @@
+package com.example.douglas.agendafirebase.config;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ConfiguracaoFirebase {
+
+    private static FirebaseAuth autenticacao;
+    private static DatabaseReference referenciaFirebase;
+
+    public static DatabaseReference getFirebase(){
+
+        if( referenciaFirebase == null ){
+            referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+        }
+
+        return referenciaFirebase;
+    }
+
+    public static FirebaseAuth getFirebaseAutenticacao(){
+        if( autenticacao == null ){
+            autenticacao = FirebaseAuth.getInstance();
+        }
+        return autenticacao;
+    }
+
+}
