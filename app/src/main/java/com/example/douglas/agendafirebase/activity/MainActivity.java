6623d80e532597f484cdc556d65c99c4a539b669
douglas-augusto <douglas-augusto@hotmail.com>
2018-09-25 11:38:58 -0300

    Logando e deslogando corrigidos, Menu feito

--- app/src/main/java/com/example/douglas/agendafirebase/activity/MainActivity.java
+++ app/src/main/java/com/example/douglas/agendafirebase/activity/MainActivity.java
@@ -1,15 +1,52 @@
 package com.example.douglas.agendafirebase.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.douglas.agendafirebase.R;
+import com.example.douglas.agendafirebase.config.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth usuarioFirebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usuarioFirebase = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+    }
+
+    private void deslogarUsuario(){
+        usuarioFirebase.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch ( item.getItemId() ){
+            case R.id.item_sair :
+                deslogarUsuario();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
